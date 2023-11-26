32bdc3d616ae2a5393da16439fbe3fae83427280
aefayoub <aef.ayoub@gmail.com>
2023-11-26 09:20:16 -0800

    cmt task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# SSH confih changing
+exec {'echo':
+  path    => 'uss/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1]
+}
