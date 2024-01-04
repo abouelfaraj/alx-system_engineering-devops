bd2ed51f008cfeb045eff901249e798f12dfe337
= <=>
2024-01-04 08:19:52 -0800

    cmt task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     list_to_json = []
     for task in tasks:
         list_dic = {"task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": json_res.get("username")}
+                    "completed": task.get("completed"),
+                    "username": json_res.get("username")}
         list_to_json.append(list_dic)
     str_list = {str(sys.argv[1]): list_to_json}
     csv_filename = "{}.json".format(sys.argv[1])
