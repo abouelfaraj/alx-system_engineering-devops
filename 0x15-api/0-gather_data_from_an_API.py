cf41a6b1ab8cd3a05fa9746eccda901b7e88324e
= <=>
2024-01-04 13:59:55 -0800

    cmt task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,10 +10,9 @@ if __name__ == "__main__":
     user = '{}users/{}'.format(url, userid)
     res = requests.get(user)
     json_res = res.json()
-    print("Employee {} is done with tasks".format(
-        json_res.get('name')), end="")
+    name = json_res.get('name')
 
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    todos = '{}todos?userId={}'.format(url, userid)
     res = requests.get(todos)
     tasks = res.json()
     listtask = []
@@ -21,6 +20,6 @@ if __name__ == "__main__":
         if task.get('completed') is True:
             listtask.append(task)
 
-    print("({}/{}):".format(len(listtask), len(tasks)))
+    print("Employee {} is done with tasks({}/{}):".format(name, len(listtask), len(tasks)))
     for task in listtask:
         print("\t {}".format(task.get("title")))
