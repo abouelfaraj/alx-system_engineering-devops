7b7acfb80d444a7fca56d351944707ebdcaa484e
= <=>
2024-01-04 05:45:21 -0800

    cmt task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that returns information about an employee TODO list progress."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_res = res.json()
+    print("Employee {} is done with tasks".format(json_res.get('name')), end="")
+
+    todos = '{}todos?user={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    listtask = []
+    for task in tasks:
+        if task.get('completed') is True:
+            listtask.append(task)
+
+    print("({}/{}):".format(len(listtask), len(tasks)))
+    for task in listtask:
+        print("\t {}".format(task.get("title")))
