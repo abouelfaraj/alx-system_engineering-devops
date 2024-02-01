dae7338636395298d0bedd8bfed162541451d37e
abouelfaraj <aef.ayoub@gmail.com>
2024-02-01 01:25:10 -0800

    cmt changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import configparser
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """Get count of subscribers"""
     config = configparser.ConfigParser()
     config.read('config.txt')
@@ -17,17 +17,28 @@ def number_of_subscribers(subreddit):
             'username': username,
             'password': password,
             }
+
+    params = {
+            'limit': 10
+            }
+
     auth = (client_id, client_secret)
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'AdvanceAPI-1.0.0'}
     response = requests.get(url=url,
                             data=data,
+                            params=params,
                             auth=auth,
                             headers=headers,
                             allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers_count = data['data']['subscribers']
-        return subscribers_count
+        children_posts = data['data']['children']
+        if len(children_posts) == 0:
+            print(None)
+        else:
+            for post in children_posts:
+                print(post['data']['title'])
     else:
+        print(None)
         return 0
